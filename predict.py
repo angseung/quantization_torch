83d2009930d748d360e436bbcd5cc8c380f5ed03
angseung <angseung@outlook.com>
2023-06-20 11:06:39 +0900

    Updated deprecated codes in predict.py

--- predict.py
+++ predict.py
@@ -1,18 +1,12 @@
-from utils.quantization_utils import QuantizableModel
 import copy
 import time
-from models.densenet import *
-from models.resnet import *
 import torch
+from models.resnet import *
+from utils.quantization_utils import cal_mse
 
-# model = densenet121(DenseNet121_Weights.DEFAULT).eval()
-# model = densenet161(DenseNet161_Weights.DEFAULT).eval()
-# model = densenet169(DenseNet169_Weights.DEFAULT).eval()
-model = resnet152().eval()
+model = resnet152(quantize=True, is_qat=False)
 model_fp = copy.deepcopy(model)
 input = torch.randn(1, 3, 224, 224)
-model.fuse_model()
-model = QuantizableModel(model).prepare()
 model(input)
 torch.ao.quantization.convert(model, inplace=True)
 start = time.time()
@@ -20,3 +14,4 @@ dummy_output = model(input)
 end = time.time() - start
 print(end)
 dummy_output_fp = model_fp(input)
+nmse = cal_mse(dummy_output, dummy_output_fp, norm=True)
